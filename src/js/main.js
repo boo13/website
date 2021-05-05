799eb396e66433ff18971498bce36ffe597c2852
boo 13 <boo13bot@gmail.com>
2021-05-05 17:27:12 -0400

    file cleanup

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+// console.log("Main.js runs");
+
+// Shorter document ready event...
+// $(function() {
+//     some JQuery methods go here'
+//     console.log("Hiding H2 tags via main.js")
+//     $("h2").hide()
+//     $("h3").hide()
+// });
