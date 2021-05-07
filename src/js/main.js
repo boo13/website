e1d816e2a6a1092ca3ee8597ae7652c3218565a7
boo 13 <boo13bot@gmail.com>
2021-05-07 01:43:55 -0400

    more css organizing

--- src/js/main.js
+++ src/js/main.js
@@ -8,63 +8,66 @@
 //     $("h3").hide()
 // });
 
-function delay(n) {
-    n = n || 2000;
-    return new Promise((done) => {
-        setTimeout(() => {
-            done();
-        }, n);
-    });
-}
 
-function pageTransition() {
-    var tl = gsap.timeline();
-    tl.to(".loading-screen", {
-        duration: 1.2,
-        width: "100%",
-        left: "0%",
-        ease: "Expo.easeInOut",
-    });
+// Barba
 
-    tl.to(".loading-screen", {
-        duration: 1,
-        width: "100%",
-        left: "100%",
-        ease: "Expo.easeInOut",
-        delay: 0.3,
-    });
-    tl.set(".loading-screen", { left: "-100%" });
-}
+// function delay(n) {
+//     n = n || 2000;
+//     return new Promise((done) => {
+//         setTimeout(() => {
+//             done();
+//         }, n);
+//     });
+// }
 
-function contentAnimation() {
-    var tl = gsap.timeline();
-    tl.from(".animate-this", { duration: 1, y: 30, opacity: 0, stagger: 0.5, delay: 0.2 });
-    // var t2 = gsap.timeline();
-    // t2.from(".animate-credits", { duration: 1, y: 30, opacity: 0, stagger: 1, delay: 0.2 });
-}
+// function pageTransition() {
+//     var tl = gsap.timeline();
+//     tl.to(".loading-screen", {
+//         duration: 1.2,
+//         width: "100%",
+//         left: "0%",
+//         ease: "Expo.easeInOut",
+//     });
 
-$(function () {
-    barba.init({
-        sync: true,
+//     tl.to(".loading-screen", {
+//         duration: 1,
+//         width: "100%",
+//         left: "100%",
+//         ease: "Expo.easeInOut",
+//         delay: 0.3,
+//     });
+//     tl.set(".loading-screen", { left: "-100%" });
+// }
 
-        transitions: [
-            {
-                async leave(data) {
-                    const done = this.async();
+// function contentAnimation() {
+//     var tl = gsap.timeline();
+//     tl.from(".animate-this", { duration: 1, y: 30, opacity: 0, stagger: 0.5, delay: 0.2 });
+//     // var t2 = gsap.timeline();
+//     // t2.from(".animate-credits", { duration: 1, y: 30, opacity: 0, stagger: 1, delay: 0.2 });
+// }
 
-                    pageTransition();
-                    await delay(1000);
-                    done();
-                },
+// $(function () {
+//     barba.init({
+//         sync: true,
 
-                async enter(data) {
-                    contentAnimation();
-                },
+//         transitions: [
+//             {
+//                 async leave(data) {
+//                     const done = this.async();
 
-                async once(data) {
-                    contentAnimation();
-                },
-            },
-        ],
-    });
-});
+//                     pageTransition();
+//                     await delay(1000);
+//                     done();
+//                 },
+
+//                 async enter(data) {
+//                     contentAnimation();
+//                 },
+
+//                 async once(data) {
+//                     contentAnimation();
+//                 },
+//             },
+//         ],
+//     });
+// });
