f55cd06a5185bd1e1d7d3cd97ebfa0328a3633ba
boo 13 <boo13bot@gmail.com>
2021-05-09 00:10:43 -0400

    temp deploy

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,18 @@
-barba.init({
-    transitions: [{
-      name: 'opacity-transition',
-      leave(data) {
-        return gsap.to(data.current.container, {
-          opacity: 0
-        });
-      },
-      enter(data) {
-        return gsap.from(data.next.container, {
-          opacity: 0
-        });
-      }
-    }]
-  });
-
+// barba.init({
+//     transitions: [{
+//       name: 'opacity-transition',
+//       leave(data) {
+//         return gsap.to(data.current.container, {
+//           opacity: 0
+//         });
+//       },
+//       enter(data) {
+//         return gsap.from(data.next.container, {
+//           opacity: 0
+//         });
+//       }
+//     }]
+//   });
 
 
 
