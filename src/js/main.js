7828075e489b549658002d3c054d4e6a5670828f
boo 13 <boo13bot@gmail.com>
2021-05-09 00:03:59 -0400

    more updated

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,23 @@
+barba.init({
+    transitions: [{
+      name: 'opacity-transition',
+      leave(data) {
+        return gsap.to(data.current.container, {
+          opacity: 0
+        });
+      },
+      enter(data) {
+        return gsap.from(data.next.container, {
+          opacity: 0
+        });
+      }
+    }]
+  });
+
+
+
+
+
 // console.log("Main.js runs");
 
 // Shorter document ready event...
