bf5c16c94dd0326d11eac5a72f903a5972d9160d
Randy <boo13bot@gmail.com>
2026-02-08 16:29:31 -0500

    del credits.html

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,7 @@ export default defineConfig({
       input: {
         main: resolve(import.meta.dirname, 'index.html'),
         index2: resolve(import.meta.dirname, 'index2.html'),
-        work: resolve(import.meta.dirname, 'work.html'),
         contact: resolve(import.meta.dirname, 'contact.html'),
-        credits: resolve(import.meta.dirname, 'credits.html'),
         resume: resolve(import.meta.dirname, 'resume.html'),
         examplesMockup: resolve(import.meta.dirname, 'examples_mockup.html'),
         sandbox: resolve(import.meta.dirname, 'sandbox.html')
